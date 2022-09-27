8b9f1bd892e67b06b3d5c2097a31c4fad64630dd
osjimene <81704872+osjimene@users.noreply.github.com>
2022-09-27 14:12:12 -0500

    Script to pull ObjectIds for user devices
    Utilizes the microsoft Graph API to get ObjectId information for Power Automate flow.

---
+++ GetObjectIds.py
@@ -0,0 +1,34 @@
+import requests
+import pandas as pd
+import numpy as np
+import easygui
+import json
+import openpyxl
+
+
+secretkey= easygui.enterbox(msg="Please enter your GraphAPI Key...")
+url = "https://graph.microsoft.com/v1.0/devices?"
+
+# path = Powerbi Tracker.xlsx"
+path = easygui.fileopenbox(msg="Please select the file you want to import")
+df = pd.read_excel(path, engine='openpyxl')
+
+def response(device):
+    endpoint = f"https://graph.microsoft.com/v1.0/devices?$select=id,deviceId,displayName&$filter=deviceId%20eq%20%27{device}%27"
+    secret = 'Bearer '+ secretkey
+    http_headers = {'Authorization' : secret,
+                    'ConsistencyLevel': 'eventual'}
+    r = requests.get(endpoint, headers = http_headers).json()
+    try:
+        return r['value'][0]['id']
+    except IndexError:
+        return "IndexError"
+    except KeyError:
+        return "KeyError"
+
+
+df['ObjectId'] = df.apply(lambda x: response(f'{x.AzureADId}'), axis=1)
+
+outfile = easygui.filesavebox(msg="Please choose where you want the outfile saved")
+
+df.to_excel(outfile,engine='openpyxl')
